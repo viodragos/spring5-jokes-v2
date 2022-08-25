673eaec642aa1dadb8ebbfb6fcba4d1af97c72c1
Viorel Dragos <viodragos@gmail.com>
2022-08-25 11:29:58 +0300

    Initial release

---
+++ src/test/java/vio/springframework/spring5jokesappv2/Spring5JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package vio.springframework.spring5jokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
